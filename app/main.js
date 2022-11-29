32c7293e7ea15aebf62d1547cac0d4809fe95e96
Alessandro Rocchi <alessandro.rocchi2000@gmail.com>
2022-11-29 20:41:39 +0100

    Update node server

--- app/main.js
+++ app/main.js
@@ -1,7 +1,22 @@
-const ronin = require('ronin-server')
-const mocks = require('ronin-mocks')
+// const ronin = require('ronin-server')
+// const mocks = require('ronin-mocks')
 
-const server = ronin.server()
+// const server = ronin.server()
 
-server.use('/', mocks.server(server.Router(), false, true))
-server.start()
+// server.use('/', mocks.server(server.Router(), false, true))
+// server.start()
+
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
